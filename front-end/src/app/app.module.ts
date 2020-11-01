e89ee3be7632966cf78a2f3dfaab990ba382a721
ddoniddoni <psdkei@naver.com>
2020-11-01 19:30:31 +0900

    Add Header

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
   ],
   imports: [
     BrowserModule,
